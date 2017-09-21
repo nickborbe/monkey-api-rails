d554efa3ada1ca0b181faa7e7777dfa1696ea5e2
Nicholas Borbe <nickborbe@gmail.com>
2017-09-21 16:22:11 -0400

    tamarins

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,10 @@ Most marmosets are about 20 centimetres (8 in) long. Relative to other monkeys, 
 social_order: "monogamy, polygyny, and polyandry - cooperative in raising offspring", lifespan: "12 Years", latin_name: "Callithrix jacchus", seeded?: true, sub_family: "Callitrichidae")
 
 
-ApiMonkey.create(name: "", height: "", weight: "",
-locations_of_origin: "", diet: ""
-description: "",
-social_order: "", lifespan: "", latin_name: "", seeded?: true, sub_family: "Callitrichidae")
+ApiMonkey.create(name: "Tamarin", height: "5-12in", weight: "7-32oz",
+locations_of_origin: "Colombia, the Amazon basin, and the Guianas", diet: "Omnivores - eating fruits and other plant parts as well as spiders, insects, small vertebrates and bird eggs."
+description: "Different tamarin species vary considerably in appearance, ranging from nearly all black through mixtures of black, brown and white. Mustache-like facial hairs are typical for many species. Their body size ranges from 13 to 30 cm (5.1 to 11.8 in) (plus a 25-to-44 cm-long (9.8-to-17.3 in) tail) and they weigh from 220 to 900 grams (7.8 to 31.7 oz). Tamarins differ from marmosets primarily in having lower canine teeth that are clearly longer than the incisors. In captivity, tamarins can live for up to 18 years. They are diurnal and arboreal, and run and jump quickly through the trees. Tamarins live together in groups of up to 40 members consisting of one or more families. More frequently, though, groups are composed of just three to nine members.",
+social_order: "Groups of 3-40 members, composed of one, or several families.  The adult males, subadults, and juveniles in the group assist with caring for the young.", lifespan: "15 years", latin_name: "Saguinus", seeded?: true, sub_family: "Callitrichidae")
 
 ApiMonkey.create(name: "", height: "", weight: "",
 locations_of_origin: "", diet: ""
