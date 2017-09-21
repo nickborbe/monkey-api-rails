4c60ca1909145e071c2de084f077947ad6515545
Nicholas Borbe <nickborbe@gmail.com>
2017-09-21 16:52:27 -0400

    spider monkey

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,33 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-ApiMonkey.create(name: "Marmosets", height: "7.5in", weight: "9oz",
+ApiMonkey.create(name: "Marmoset", height: "7.5in", weight: "9oz",
 locations_of_origin: "Bolivia, Brazil, Colombia, Ecuador, Paraguay, and Peru.", diet: "Mostly Tree Bark. Also fruit, seeds, flowers, fungi, nectar, snails, lizards, tree frogs, bird eggs, nestlings, and infant mammals."
 description: "The marmosets also known as zaris (/ˈmɑːrməˌzɛts, -ˌsɛts/[3][4]) are 22 New World monkey species of the genera Callithrix, Cebuella, Callibella, and Mico. All four genera are part of the biological family Callitrichidae. The term marmoset is also used in reference to the Goeldi's marmoset, Callimico goeldii, which is closely related.
 Most marmosets are about 20 centimetres (8 in) long. Relative to other monkeys, they show some apparently primitive features: they have claws rather than nails, and tactile hairs on their wrists. They lack wisdom teeth, and their brain layout seems to be relatively primitive. Their body temperature is unusually variable, changing by up to 4 °C (7 °F) in a day.",
 social_order: "monogamy, polygyny, and polyandry - cooperative in raising offspring", lifespan: "12 Years", latin_name: "Callithrix jacchus", seeded?: true, family: "Callitrichidae")
 
 
-ApiMonkey.create(name: "Tamarins", height: "5-12in", weight: "7-32oz",
+ApiMonkey.create(name: "Tamarin", height: "5-12in", weight: "7-32oz",
 locations_of_origin: "Colombia, the Amazon basin, and the Guianas", diet: "Omnivores - eating fruits and other plant parts as well as spiders, insects, small vertebrates and bird eggs."
 description: "Different tamarin species vary considerably in appearance, ranging from nearly all black through mixtures of black, brown and white. Mustache-like facial hairs are typical for many species. Their body size ranges from 13 to 30 cm (5.1 to 11.8 in) (plus a 25-to-44 cm-long (9.8-to-17.3 in) tail) and they weigh from 220 to 900 grams (7.8 to 31.7 oz). Tamarins differ from marmosets primarily in having lower canine teeth that are clearly longer than the incisors. In captivity, tamarins can live for up to 18 years. They are diurnal and arboreal, and run and jump quickly through the trees. Tamarins live together in groups of up to 40 members consisting of one or more families. More frequently, though, groups are composed of just three to nine members.",
 social_order: "Groups of 3-40 members, composed of one, or several families.  The adult males, subadults, and juveniles in the group assist with caring for the young.", lifespan: "15 years", latin_name: "Saguinus", seeded?: true, family: "Callitrichidae")
 
-ApiMonkey.create(name: "Squirrel Monkeys", height: "1ft", weight: "0.5lb",
+ApiMonkey.create(name: "Squirrel Monkey", height: "1ft", weight: "0.5lb",
 locations_of_origin: "Amazon, Costa Rica, Panama", diet: "Mostly fruit and insects"
 description: "Squirrel monkey fur is short and close, coloured black at the shoulders and yellowish orange on its back and extremities. Their throats and ears are white, and their mouths are black. The upper parts of their heads are hairy. This black-and-white face gives them the name 'death's head monkey' in several Germanic languages.
 Like most of their New World monkey relatives, squirrel monkeys are diurnal and arboreal. Unlike other New World monkeys, their tail is not used for climbing but as a kind of 'balancing pole' and also as a tool. Their movements in the branches can be very rapid.",
 social_order: "Multi-male/multi-female groups with up to 500 members", lifespan: "15-20 years", latin_name: "Saimiriinae", seeded?: true, family: "Cebidae")
 
-ApiMonkey.create(name: "Capuchin Monkeys", height: "12-22in", weight: "2-3lbs",
+ApiMonkey.create(name: "Capuchin Monkey", height: "12-22in", weight: "2-3lbs",
 locations_of_origin: "Central America and South America as far south as northern Argentina", diet: "Omnivorous - they feed on fruits, nuts and seeds, berries, insects, lizards, rodents and small birds"
 description: "Capuchins are often considered the most intelligent New World monkeys and are often used in laboratories. The tufted capuchin is especially noted for its long-term tool usage",
-social_order: "Groups of 10 to 35 individuals.  Usually 1 dominant male, some Capuchin species have 1 dominant female as well.", lifespan: "44 years", latin_name: "Cebinae", seeded?: true, family: "Cebidae")
+social_order: "Groups of 10 to 35 individuals.  Usually 1 dominant male, some Capuchin species have 1 dominant female as well.", lifespan: "22 years", latin_name: "Cebinae", seeded?: true, family: "Cebidae")
 
-ApiMonkey.create(name: "", height: "", weight: "",
-locations_of_origin: "", diet: ""
-description: "",
-social_order: "", lifespan: "", latin_name: "", seeded?: true, family: "Callitrichidae")
+ApiMonkey.create(name: "Spider Monkey", height: "26in", weight: "20lbs",
+locations_of_origin: "tropical forests of Central and South America, from southern Mexico to Brazil", diet: ""
+description: "Spider monkeys are among the largest New World monkeys; black-headed spider monkeys, the largest spider monkey, have an average weight of 11 kilograms (24 lb) for males and 9.66 kg (21.3 lb) for females.[9][10] Disproportionately long, spindly limbs inspired the spider monkey's common name. Their deftly prehensile tails,[11] which may be up to 89 cm (35 in) long, have very flexible, hairless tips and skin grooves similar to fingerprints. This adaptation to their strictly arboreal lifestyle serves as a fifth hand.[12] When the monkey walks, its arms practically drag on the ground. Unlike many monkeys, they do not use their arms for balance when walking, instead relying on their tails. The hands are long, narrow and hook-like, and have reduced or non-existent thumbs.[13] The fingers are elongated and recurved.",
+social_order: "Spider monkeys form loose groups of 15 to 25 individuals,[21] but can have even 30[22] to 40.[23] During the day, groups break up into subgroups of two to eight animals. This social structure (fission-fusion) is found in only two other types of primates: chimpanzees and Homo sapiens", lifespan: "40 years", latin_name: "Atelinae", seeded?: true, family: "Atelidae")
 
 ApiMonkey.create(name: "", height: "", weight: "",
 locations_of_origin: "", diet: ""
