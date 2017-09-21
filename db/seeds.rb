b62dcbe842c1152550a031792213ff8e485a8ebd
Nicholas Borbe <nickborbe@gmail.com>
2017-09-21 16:11:51 -0400

    marmoset

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-ApiMonkey.create(name
-height
-weight
-locations_of_origin
-diet
-description
-social_order
-lifespan
-latin_name
-seeded?
-sub_family)
+ApiMonkey.create(name: "Marmoset", height: "7.5in", weight: "9oz",
+locations_of_origin: "Bolivia, Brazil, Colombia, Ecuador, Paraguay, and Peru.", diet: "Mostly Tree Bark. Also fruit, seeds, flowers, fungi, nectar, snails, lizards, tree frogs, bird eggs, nestlings, and infant mammals."
+description: "The marmosets also known as zaris (/ˈmɑːrməˌzɛts, -ˌsɛts/[3][4]) are 22 New World monkey species of the genera Callithrix, Cebuella, Callibella, and Mico. All four genera are part of the biological family Callitrichidae. The term marmoset is also used in reference to the Goeldi's marmoset, Callimico goeldii, which is closely related.
+Most marmosets are about 20 centimetres (8 in) long. Relative to other monkeys, they show some apparently primitive features: they have claws rather than nails, and tactile hairs on their wrists. They lack wisdom teeth, and their brain layout seems to be relatively primitive. Their body temperature is unusually variable, changing by up to 4 °C (7 °F) in a day.",
+social_order: "monogamy, polygyny, and polyandry - cooperative in raising offspring", lifespan: "12 Years", latin_name: "Callithrix jacchus", seeded?: true, sub_family: "Callitrichidae")
+
+
+ApiMonkey.create(name: "", height: "", weight: "",
+locations_of_origin: "", diet: ""
+description: "",
+social_order: "", lifespan: "", latin_name: "", seeded?: true, sub_family: "Callitrichidae")
+
+ApiMonkey.create(name: "", height: "", weight: "",
+locations_of_origin: "", diet: ""
+description: "",
+social_order: "", lifespan: "", latin_name: "", seeded?: true, sub_family: "Callitrichidae")
+
+ApiMonkey.create(name: "", height: "", weight: "",
+locations_of_origin: "", diet: ""
+description: "",
+social_order: "", lifespan: "", latin_name: "", seeded?: true, sub_family: "Callitrichidae")
+
+ApiMonkey.create(name: "", height: "", weight: "",
+locations_of_origin: "", diet: ""
+description: "",
+social_order: "", lifespan: "", latin_name: "", seeded?: true, sub_family: "Callitrichidae")
+
+ApiMonkey.create(name: "", height: "", weight: "",
+locations_of_origin: "", diet: ""
+description: "",
+social_order: "", lifespan: "", latin_name: "", seeded?: true, sub_family: "Callitrichidae")
+
+ApiMonkey.create(name: "", height: "", weight: "",
+locations_of_origin: "", diet: ""
+description: "",
+social_order: "", lifespan: "", latin_name: "", seeded?: true, sub_family: "Callitrichidae")
