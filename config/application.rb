11ff5d91e67a105da81ba1564549c5fc00bdd0e7
Nicholas Borbe <nickborbe@gmail.com>
2017-09-21 23:10:37 -0400

    add CORS code to application controller:

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,26 @@ Bundler.require(*Rails.groups)
 module MonkeyCityApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    # config.load_defaults 5.1
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
+    # Rails 3/4
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
+  end
+end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
