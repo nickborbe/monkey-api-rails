e5baafbf7371e595a12c966bd12f58bc67a54674
Nicholas Borbe <nickborbe@gmail.com>
2017-09-22 11:49:55 -0400

    clean up

--- config/application.rb
+++ config/application.rb
@@ -17,19 +17,5 @@ module MonkeyCityApi
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
-
-    # Rails 3/4
-
-    # config.middleware.insert_before 0, "Rack::Cors" do
-    #   allow do
-    #     origins '*'
-    #     resource '*', :headers => :any, :methods => [:get, :post, :options]
-    #   end
-    end
-
-
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
+end
