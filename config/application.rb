9fa75e0bfebec9270efb368f74be02cb579f245a
Nicholas Borbe <nickborbe@gmail.com>
2017-09-21 23:18:21 -0400

    CORS

--- config/application.rb
+++ config/application.rb
@@ -20,18 +20,16 @@ module MonkeyCityApi
 
     # Rails 3/4
 
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
+    # config.middleware.insert_before 0, "Rack::Cors" do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :options]
+    #   end
     end
 
-  end
-end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-end
