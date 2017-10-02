ce64be554bd813ed80743f1f6ce470d3e3362683
Nicholas Borbe <nickborbe@gmail.com>
2017-10-01 22:31:21 -0400

    tamarin marmoset images added

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,13 @@ ApiMonkey.create(name: "Marmoset", height: "7.5 in", weight: "9 oz",
 locations_of_origin: "Bolivia, Brazil, Colombia, Ecuador, Paraguay, and Peru.", diet: "Mostly Tree Bark. Also fruit, seeds, flowers, fungi, nectar, snails, lizards, tree frogs, bird eggs, nestlings, and infant mammals.",
 description: "The marmosets are 22 New World monkey species of the genera Callithrix, Cebuella, Callibella, and Mico. All four genera are part of the biological family Callitrichidae. The term marmoset is also used in reference to the Goeldi's marmoset, Callimico goeldii, which is closely related.
 Most marmosets are about 20 centimetres (8 in) long. Relative to other monkeys, they show some apparently primitive features: they have claws rather than nails, and tactile hairs on their wrists. They lack wisdom teeth, and their brain layout seems to be relatively primitive. Their body temperature is unusually variable, changing by up to 4 °C (7 °F) in a day.",
-social_order: "monogamy, polygyny, and polyandry - cooperative in raising offspring", lifespan: "12 Years", genus: "Callithrix", seeded?: true, sub_family: "Callitrichidae", image1: "", image2:"")
+social_order: "monogamy, polygyny, and polyandry - cooperative in raising offspring", lifespan: "12 Years", genus: "Callithrix", seeded?: true, sub_family: "Callitrichidae", image1: "marmoset.jpg", image2:"marmoset2.jpg")
 
 
 ApiMonkey.create(name: "Tamarin", height: "5-12 in", weight: "7-32 oz",
 locations_of_origin: "Colombia, the Amazon basin, and the Guianas", diet: "Omnivores - eating fruits and other plant parts as well as spiders, insects, small vertebrates and bird eggs.",
 description: "Different tamarin species vary considerably in appearance, ranging from nearly all black through mixtures of black, brown and white. Mustache-like facial hairs are typical for many species. Their body size ranges from 13 to 30 cm (5.1 to 11.8 in) (plus a 25-to-44 cm-long (9.8-to-17.3 in) tail) and they weigh from 220 to 900 grams (7.8 to 31.7 oz). Tamarins differ from marmosets primarily in having lower canine teeth that are clearly longer than the incisors. In captivity, tamarins can live for up to 18 years. They are diurnal and arboreal, and run and jump quickly through the trees. Tamarins live together in groups of up to 40 members consisting of one or more families. More frequently, though, groups are composed of just three to nine members.",
-social_order: "Groups of 3-40 members, composed of one, or several families.  The adult males, subadults, and juveniles in the group assist with caring for the young.", lifespan: "15 years", genus: "Saguinus", seeded?: true, sub_family: "Callitrichidae", image1: "", image2:"")
+social_order: "Groups of 3-40 members, composed of one, or several families.  The adult males, subadults, and juveniles in the group assist with caring for the young.", lifespan: "15 years", genus: "Saguinus", seeded?: true, sub_family: "Callitrichidae", image1: "tamarin.jpg", image2:"tamarin2.jpg")
 
 ApiMonkey.create(name: "Squirrel Monkey", height: "1 ft", weight: "0.5 lbs",
 locations_of_origin: "Amazon, Costa Rica, Panama", diet: "Mostly fruit and insects",
